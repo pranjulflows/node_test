00af5da0094047f0a5e5ff17f9f8bee042bc553f
Pranjul Dhiman <pranjuldheeman@gmail.com>
2023-11-24 23:22:45 +0530

    added the netlify deployement

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const serverless = require('serverless-http');
 require('dotenv').config()
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -23,7 +23,7 @@ app.get('/youtube', (req, res) => {
 })
 
 app.listen(process.env.PORT, () => {
-    console.log(`Example app listening on port ${process.env.PORT}`)
+    console.log(`Example app listening on port ${port}`)
 })
 module.exports = app;
 
